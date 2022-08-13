a6a84f2aeb6b70571bede932441a6368d40287e5
Simrat Grewal <simrats169169@gmail.com>
2022-08-12 20:42:23 -0700

    feat: Add copy icon to homepage

--- src/Plugin.tsx
+++ src/Plugin.tsx
@@ -1,10 +1,22 @@
 import styles from "./styles/plugin.css"
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const Plugin = (props: { title: string; desc: string, key: React.Key }) => {
   return (
     <div className={styles.pluginCard}>
-      <div className={styles.pluginTitle}>{props.title}</div>
-      <div className={styles.pluginDesc}>{props.desc}</div>
+      <div className={styles.text}>
+        <div className={styles.pluginTitle}>{props.title}</div>
+        <div className={styles.pluginDesc}>{props.desc}</div>
+      </div>
+      <div className={styles.end}>
+        <button className={styles.copy} onClick={
+          () => navigator.clipboard.writeText(props.title)
+        }>
+          <FontAwesomeIcon icon={faCopy} />
+        </button>
+      </div>
     </div>
   )
 }
